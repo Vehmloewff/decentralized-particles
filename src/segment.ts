5545200320f7cb883a35a305f0901eb6672d9fc8
Elijah <ejsisgreat@gmail.com>
2020-02-06 08:30:54 -0600

    Add a Segment class

---
+++ src/segment.ts
@@ -0,0 +1,23 @@
+import { Particle } from './particle';
+
+export interface SegmentOptions {
+	stroke?: string;
+	width?: string;
+	strength?: number; // between 0 and 1
+}
+
+export class Segment {
+	options: SegmentOptions;
+	startParticle: Particle;
+	endParticle: Particle;
+
+	constructor(startParticle: Particle, endParticle: Particle, options?: SegmentOptions) {
+		const errBase = `The first param to 'Segment' `;
+		if (!(startParticle instanceof Particle && endParticle instanceof Particle))
+			throw new Error(errBase + `must be of type '[Particle, Particle]'`);
+
+		this.options = options;
+		this.startParticle = startParticle;
+		this.endParticle = endParticle;
+	}
+}
